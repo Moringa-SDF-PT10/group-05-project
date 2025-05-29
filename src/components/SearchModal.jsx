a89f3378eef8fede6186d47c1266cac4aeab7c4f
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-05-29 16:04:10 +0300

    Add movie search logic with loading spinner and results display

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Modal, Form, Button, Row, Col } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Modal, Form, Button, Row, Col,Spinner } from "react-bootstrap";
 import MovieCard from "./MovieCard";
 
 function SearchModal({ show, handleClose, searchMovies }) {
@@ -7,13 +7,37 @@ function SearchModal({ show, handleClose, searchMovies }) {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (!query.trim())
+      return
+    setIsLoading(true)
+    try{
+   
+      const data = await searchMovies(query)
+      setResults(data)
+    }catch(error){
+     console.error("Search error:", error)
+    }finally{
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+  if (!show) {
+    setQuery("");
+    setResults([]);
+  }
+}, [show]);
+
+
   return (
     <Modal show={show} onHide={handleClose} size="lg">
       <Modal.Header closeButton>
         <Modal.Title>Search Movies</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form>
+        <Form onSubmit={handleSearch}>
           <div className="d-flex mb-4">
             <Form.Control
               type="text"
@@ -27,11 +51,23 @@ function SearchModal({ show, handleClose, searchMovies }) {
               className="ms-2"
               disabled={isLoading}
             >
-              {isLoading ? "Searching..." : "Search"}
+              {isLoading ? <Spinner animation="border" size="sm" />  : "Search"}
             </Button>
           </div>
         </Form>
-        <p>Harriet Load you movie here below</p>
+         {results.length > 0 && (
+           <Row xs={1} sm={2} md={3} lg={4} xl={5} className="g-4">
+           {results.map((movie) => (
+           <Col key={movie.id}>
+            <MovieCard movie={movie} />
+           </Col>
+          ))}
+          </Row>
+        )}
+
+         {!isLoading && results.length === 0 && query.trim() && (
+         <p>No movies found for "{query}".</p>
+        )}
       </Modal.Body>
     </Modal>
   );
